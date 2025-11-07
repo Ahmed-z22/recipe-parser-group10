a85fd01bc92d13664984c48655a33122c6491656
Ahmed-z22 <ahmed.z9390@hotmail.com>
2025-11-06 18:19:56 -0600

    Wrote the initial architecture of the ingredients class

---
+++ src/ingredients_parser.py
@@ -0,0 +1,26 @@
+class IngredientsParser:
+    def __init__(self, ingredients_list):
+        self.ingredients_list = ingredients_list
+        self.names = None
+        self.quantities = None
+        self.units = None
+        self.descriptor = None
+        self.preparation = None
+
+    def extract_names(self):
+        pass
+
+    def extract_quantities(self):
+        pass
+
+    def extract_units(self):
+        pass
+
+    def extract_descriptor(self):
+        pass
+
+    def extract_preparation(self):
+        pass
+
+    def answers():
+        pass
